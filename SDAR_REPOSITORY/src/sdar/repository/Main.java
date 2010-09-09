4c6d28fbfb990e332712757ce36d4734e4137846
rockhoundi <rockhoundi@2696e7a0-b854-bc65-fdc1-a136afedb11f>
2010-09-09 19:19:01 +0000

    Nivel mais baixo do sisteman SDAR

---
+++ SDAR_REPOSITORY/src/sdar/repository/Main.java
@@ -0,0 +1,23 @@
+/**
+ * Main.java
+ * cadi
+ * SDAR_REPOSITORY
+ * sdar.repository
+ */
+package sdar.repository;
+
+/**
+ * @author cadi
+ *
+ */
+public class Main {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
