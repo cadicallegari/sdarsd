b9d5a9122af9c41577be955ae7d46100d4e15b9c
mateuscristiano <mateuscristiano@2696e7a0-b854-bc65-fdc1-a136afedb11f>
2010-10-01 16:38:15 +0000

    Pacotes de autenticacao e base de dados

---
+++ SDAR_MANAGER/src/sdar/manager/database/DBConnection.java
@@ -0,0 +1,51 @@
+package sdar.manager.database;
+
+import com.db4o.Db4oEmbedded;
+import com.db4o.ObjectContainer;
+
+/**
+ * Classe que faz os tratamentos refente as conexoes com a base de dados (DB4O)
+ * @author matheus
+ */
+public class DBConnection {
+	
+	private ObjectContainer objectContainer;
+	
+	/**
+	 * Metodo que efetua a conexao com a base de dados
+	 */
+	public void connect() {
+		this.objectContainer = Db4oEmbedded.openFile(Db4oEmbedded.newConfiguration(), DBEspecification.FILE_NAME);
+    }
+	
+	/**
+	 * Metodo que fecha a conexao com a base de dados
+	 */
+	public void disconnect() {
+		this.objectContainer.close();
+	}
+	
+	/**
+	 * Metodo que efetua a operacao de commit na base de dados
+	 */
+	public void commit() {
+		this.objectContainer.commit();
+	}
+	
+	/**
+	 * Metodo que retorna a conexao da base de dados
+	 * @return
+	 */
+	public ObjectContainer getObjectContainer() {
+		return this.objectContainer;
+	}
+
+
+	/**
+	 * Metodo que seta a conexao da base de dados
+	 * @param dbConnection
+	 */
+	public void setObjectContainer(ObjectContainer objectContainer) {
+		this.objectContainer = objectContainer;
+	}
+}
---
+++ SDAR_MANAGER/src/sdar/manager/database/DBEspecification.java
@@ -0,0 +1,11 @@
+package sdar.manager.database;
+
+/**
+ * Classe de Especificação de atributos da base de dados
+ * @author matheus
+ */
+public class DBEspecification {
+	
+	public static String FILE_NAME = "sdar-data.yap";
+	
+}
