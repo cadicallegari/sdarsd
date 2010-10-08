052a7c77a256be6676386d286a0c2a96391b37c6
rockhoundi <rockhoundi@2696e7a0-b854-bc65-fdc1-a136afedb11f>
2010-10-08 18:47:08 +0000

    chamando list

--- SDAR_INTERFACE/src/sdar/gui/Main.java
+++ SDAR_INTERFACE/src/sdar/gui/Main.java
@@ -29,7 +29,7 @@ import org.gnome.gtk.TreeViewColumn;
 import org.gnome.gtk.Widget;
 import org.gnome.gtk.Window;
 
-import sdar.bo.File;
+import sdar.bo.Archive;
 import sdar.bo.Person;
 import sdar.client.manager.UCManterArquivoManager;
 import sdar.comunication.def.ComEspecification;
@@ -53,12 +53,15 @@ public class Main {
 	private MenuItem menuUserConsult;
 	private MenuItem menuExit;
 	private MenuItem menuAbout;
+	
+	
 	private Label labelUpload;
 	private Label labelDownload;
 	private Button upload;
+	private Button listar;
 	private Button download;
 	private FileChooserButton fileChooserUpload;
-	private TreeView listFilesRepository;
+	private TreeView listArchivesRepository;
 	private ListStore model;
 	private DataColumnString fileName;
 	private DataColumnString fileSize;
@@ -96,7 +99,8 @@ public class Main {
 		labelDownload = (Label) gladeFile.getWidget("txt_download");
 		upload = (Button) gladeFile.getWidget("btn_upload");
 		download = (Button) gladeFile.getWidget("btn_download");
-		listFilesRepository = (TreeView) gladeFile.getWidget("lista_arquivos");
+		listar = (Button) gladeFile.getWidget("btn_listar");
+		listArchivesRepository = (TreeView) gladeFile.getWidget("lista_arquivos");
 	}
 	
 	/**
@@ -110,7 +114,7 @@ public class Main {
 			public void onActivate(MenuItem arg0) {
 				try {
 					person = new Person();
-					new Login(authentication, person, statusBar, fileChooserUpload, upload, download, listFilesRepository);
+					new Login(authentication, person, statusBar, fileChooserUpload, upload, download, listArchivesRepository);
 				} catch (FileNotFoundException e) {
 					e.printStackTrace();
 				}
@@ -206,6 +210,25 @@ public class Main {
 			}
 		});
 		
+		//Evento do botao Listar
+		listar.connect(new Button.Clicked() {
+			@Override
+			public void onClicked(Button arg0) {
+				//TODO Efetuar Listamento
+				try {
+					Registry reg = LocateRegistry.getRegistry("localhost", ComEspecification.RMI_PORT_SERVER);
+					RemoteServiceInterface stub = (RemoteServiceInterface) reg.lookup(ComEspecification.RMI_NAME);
+					System.out.println(stub.retrieveAllArchive().size());
+				} catch (RemoteException e) {
+					e.printStackTrace();
+				} catch (NotBoundException e) {
+					e.printStackTrace();
+				}
+				
+				
+			}
+		});
+		
 		//Evento relacionado ao fechamento da Janela
 		mainWindow.connect(new Window.DeleteEvent() {
 			@Override
@@ -217,15 +240,15 @@ public class Main {
 		});
 		
 		//Evento da ativacao de uma linha da tabela
-		listFilesRepository.connect(new TreeView.RowActivated() {
+		listArchivesRepository.connect(new TreeView.RowActivated() {
 			@Override
             public void onRowActivated(TreeView treeView, TreePath treePath, TreeViewColumn treeViewColumn) {
 				TreeIter row = model.getIter(treePath);
-                File file = new File();
-                file.setFilename(model.getValue(row, fileName));
-                file.setSize(Integer.valueOf(model.getValue(row, fileSize)));
+                Archive archive = new Archive();
+                archive.setFilename(model.getValue(row, fileName));
+                archive.setSize(Integer.valueOf(model.getValue(row, fileSize)));
                 labelDownload.setAlignment(0, Float.valueOf("0.5"));
-                labelDownload.setLabel(file.getFilename());
+                labelDownload.setLabel(archive.getFilename());
             }
 		});
 	}
@@ -261,7 +284,7 @@ public class Main {
 		this.fileChooserUpload.setSensitive(sensitive);
 		this.upload.setSensitive(sensitive);
 		this.download.setSensitive(sensitive);
-		this.listFilesRepository.setSensitive(sensitive);
+		this.listArchivesRepository.setSensitive(sensitive);
 	}
 	
 	/**
@@ -271,46 +294,46 @@ public class Main {
         TreeIter row = null;
         CellRendererText renderer = null;
         TreeViewColumn column = null;
-		List<File> listFiles = null;
+		List<Archive> listArchives = null;
 		
 		//Conexao RMI onde invoca metodo remoto para retornar todos os objetos Files
 		try {
 			Registry reg = LocateRegistry.getRegistry("localhost", ComEspecification.RMI_PORT_SERVER);
 			RemoteServiceInterface stub = (RemoteServiceInterface) reg.lookup(ComEspecification.RMI_NAME);
-			listFiles = stub.retrieveAllFile();
+			listArchives = stub.retrieveAllArchive();
 		} catch (RemoteException e) {
 			e.printStackTrace();
 		} catch (NotBoundException e) {
 			e.printStackTrace();
 		}
 		
-		//Transforma de Lista para Vector de File
-		File[] files = new File[0];
-		if (listFiles != null) {
-			files = new File[listFiles.size()];
-			for (int i = 0; i < listFiles.size(); i++) {
-				files[i] = listFiles.get(i);
+		//Transforma de Lista para Vector de Archive
+		Archive[] archives = new Archive[0];
+		if (listArchives != null) {
+			archives = new Archive[listArchives.size()];
+			for (int i = 0; i < listArchives.size(); i++) {
+				archives[i] = listArchives.get(i);
 			}
 		}
 		
 		//Preenche a lista de arquivos com os dados
         model = new ListStore(new DataColumn[] {fileName = new DataColumnString(), fileSize = new DataColumnString()});
-        for (File file : files) {
+        for (Archive archive : archives) {
             row = model.appendRow();
-            model.setValue(row, fileName, file.getFilename());
-            model.setValue(row, fileSize, String.valueOf(file.getSize()));
+            model.setValue(row, fileName, archive.getFilename());
+            model.setValue(row, fileSize, String.valueOf(archive.getSize()));
         }
-        listFilesRepository.setModel(model);
+        listArchivesRepository.setModel(model);
 
         
         //Seta as propriedades das colunas
-        column = listFilesRepository.appendColumn();
+        column = listArchivesRepository.appendColumn();
         column.setTitle("Nome do Arquivo");
         column.setMinWidth(300);
         renderer = new CellRendererText(column);
         renderer.setText(fileName);
 
-        column = listFilesRepository.appendColumn();
+        column = listArchivesRepository.appendColumn();
         column.setTitle("Tamanho do Arquivo");
         renderer = new CellRendererText(column);
         renderer.setText(fileSize);
@@ -321,36 +344,36 @@ public class Main {
 	 */
 	public void updateListFiles() {
 		TreeIter row = null;
-		List<File> listFiles = null;
+		List<Archive> listArchives = null;
 		
 		//Conexao RMI onde invoca metodo remoto para retornar todos os objetos Files
 		try {
 			Registry reg = LocateRegistry.getRegistry("localhost", ComEspecification.RMI_PORT_SERVER);
 			RemoteServiceInterface stub = (RemoteServiceInterface) reg.lookup(ComEspecification.RMI_NAME);
-			listFiles = stub.retrieveAllFile();
+			listArchives = stub.retrieveAllArchive();
 		} catch (RemoteException e) {
 			e.printStackTrace();
 		} catch (NotBoundException e) {
 			e.printStackTrace();
 		}
 		
-		//Transforma de Lista para Vector de File
-		File[] files = new File[0];
-		if (listFiles != null) {
-			files = new File[listFiles.size()];
-			for (int i = 0; i < listFiles.size(); i++) {
-				files[i] = listFiles.get(i);
+		//Transforma de Lista para Vector de Archive
+		Archive[] archives = new Archive[0];
+		if (listArchives != null) {
+			archives = new Archive[listArchives.size()];
+			for (int i = 0; i < listArchives.size(); i++) {
+				archives[i] = listArchives.get(i);
 			}
 		}
 		
 		//Preenche a lista de arquivos com os dados
         model = new ListStore(new DataColumn[] {fileName = new DataColumnString(), fileSize = new DataColumnString()});
-        for (File file : files) {
+        for (Archive archive : archives) {
             row = model.appendRow();
-            model.setValue(row, fileName, file.getFilename());
-            model.setValue(row, fileSize, String.valueOf(file.getSize()));
+            model.setValue(row, fileName, archive.getFilename());
+            model.setValue(row, fileSize, String.valueOf(archive.getSize()));
         }
-        listFilesRepository.setModel(model);
+        listArchivesRepository.setModel(model);
 	}
 
 	/**
