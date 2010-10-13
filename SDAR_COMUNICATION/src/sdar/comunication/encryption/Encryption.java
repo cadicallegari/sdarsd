bf2006a1ec2affbbfcc13ea7968dd8cbba4c62e7
mateuscristiano <mateuscristiano@2696e7a0-b854-bc65-fdc1-a136afedb11f>
2010-10-13 19:43:46 +0000

    Criptografia com o objeto Person

--- SDAR_COMUNICATION/src/sdar/comunication/encryption/Encryption.java
+++ SDAR_COMUNICATION/src/sdar/comunication/encryption/Encryption.java
@@ -1,5 +1,7 @@
 package sdar.comunication.encryption;
 
+import sdar.bo.Person;
+
 /**
  * Classe que contem os algoritmos de criptografia do projeto
  */
@@ -8,40 +10,108 @@ public class Encryption {
 	
     /**
      * Metodo que faz a criptografia de uma mensagem
-     * @param mensagem - Mensagem a ser criptografada
-     * @return Mensagem criptografada
+     * @param person - Objeto Person a ser criptografado
+     * @return Objeto Encriptografado
      */
-    public String encrypt(String mensagem) {
-    	String mensagemRetorno = new String();
+    public Person encrypt(Person person) {
+    	Person personReturn = new Person();
     	int valor;
     	char auxiliar;
+    	String message, messageReturn;
     	
-    	for (int i = 0; i < mensagem.length(); i++) {
-    		valor = (int) mensagem.charAt(i);
-    		valor++;
-    		auxiliar = (char) valor;
-    		mensagemRetorno = mensagemRetorno.concat(String.valueOf(auxiliar));
+    	//Encriptografa Nome da Pessoa
+    	message = person.getName();
+    	if (message != null) {
+    		messageReturn = "";
+        	for (int i = 0; i < message.length(); i++) {
+        		valor = (int) message.charAt(i);
+        		valor++;
+        		auxiliar = (char) valor;
+        		messageReturn = messageReturn.concat(String.valueOf(auxiliar));
+        	}
+        	personReturn.setName(messageReturn);
     	}
-    	return mensagemRetorno;
+    	
+    	//Encriptografa Usuario da Pessoa
+    	message = person.getUser();
+    	if (message != null) {
+    		messageReturn = "";
+        	for (int i = 0; i < message.length(); i++) {
+        		valor = (int) message.charAt(i);
+        		valor++;
+        		auxiliar = (char) valor;
+        		messageReturn = messageReturn.concat(String.valueOf(auxiliar));
+        	}
+        	personReturn.setUser(messageReturn);	
+    	}
+    	
+    	//Encriptografa Senha da Pessoa
+    	message = person.getPassword();
+    	if (message != null) {
+    		messageReturn = "";
+        	for (int i = 0; i < message.length(); i++) {
+        		valor = (int) message.charAt(i);
+        		valor++;
+        		auxiliar = (char) valor;
+        		messageReturn = messageReturn.concat(String.valueOf(auxiliar));
+        	}
+        	personReturn.setPassword(messageReturn);
+    	}
+    	
+    	return personReturn;
     }
     
     
     /**
      * Metodo que faz a descriptografia de uma mensagem
-     * @param mensagem - Mensagem a ser descriptografada
-     * @return Mensagem criptografada
+     * @param person - Objeto Person a ser descriptografado
+     * @return Objeto descriptografado
      */
-    public String decrypt(String mensagem) {
-    	String mensagemRetorno = new String();
+    public Person decrypt(Person person) {
+    	Person personReturn = new Person();
     	int valor;
     	char auxiliar;
+    	String message, messageReturn;
     	
-    	for (int i = 0; i < mensagem.length(); i++) {
-    		valor = (int) mensagem.charAt(i);
-    		valor--;
-    		auxiliar = (char) valor;
-    		mensagemRetorno = mensagemRetorno.concat(String.valueOf(auxiliar));
+    	//Descriptografa Nome da Pessoa
+    	message = person.getName();
+    	if (message != null) {
+    		messageReturn = "";
+        	for (int i = 0; i < message.length(); i++) {
+        		valor = (int) message.charAt(i);
+        		valor--;
+        		auxiliar = (char) valor;
+        		messageReturn = messageReturn.concat(String.valueOf(auxiliar));
+        	}
+        	personReturn.setName(messageReturn);	
     	}
-    	return mensagemRetorno;
+    	
+    	//Descriptografa Usuario da Pessoa
+    	message = person.getUser();
+    	if (message != null) {
+    		messageReturn = "";
+        	for (int i = 0; i < message.length(); i++) {
+        		valor = (int) message.charAt(i);
+        		valor--;
+        		auxiliar = (char) valor;
+        		messageReturn = messageReturn.concat(String.valueOf(auxiliar));
+        	}
+        	personReturn.setUser(messageReturn);
+    	}
+    	
+    	//Descriptografa Senha da Pessoa
+    	message = person.getPassword();
+    	if (message != null) {
+    		messageReturn = "";
+        	for (int i = 0; i < message.length(); i++) {
+        		valor = (int) message.charAt(i);
+        		valor--;
+        		auxiliar = (char) valor;
+        		messageReturn = messageReturn.concat(String.valueOf(auxiliar));
+        	}
+        	personReturn.setPassword(messageReturn);	
+    	}
+    	
+    	return personReturn;
     }
 }
